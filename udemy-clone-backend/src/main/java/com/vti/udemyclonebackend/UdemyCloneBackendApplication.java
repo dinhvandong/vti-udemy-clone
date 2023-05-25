9af6fd505c85824795ef0ac6817fec33774d2edb
LAPTOP-T4IEN2K1\TUF_Gaming <dongdinhvan.edu@gmail.com>
2023-05-25 09:55:46 +0700

    backend

---
+++ udemy-clone-backend/src/main/java/com/vti/udemyclonebackend/UdemyCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vti.udemyclonebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyCloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyCloneBackendApplication.class, args);
+	}
+
+}
---
+++ udemy-clone-backend/src/test/java/com/vti/udemyclonebackend/UdemyCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.udemyclonebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyCloneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
